41ea56d6e8801abc98c292f9ef81d4086ad02643
BartBachrach <bartbachrach@gmail.com>
2022-04-27 15:05:58 -0400

    module 8 work

--- ETL_class_activities.py
+++ ETL_class_activities.py
@@ -1,36 +1,3 @@
-# we own a taco stand and we want our customers to be able to 
-# choose a meat and veggie for their taco
-
-# our function prints to the kitchen the taco order
-
-def make_taco(meat, veg):
-    print(f"This taco has {meat} and {veg}.")
-
-make_taco(meat='turkey', veg='sweet potatoes')
-
-make_taco(meat='carnitas', veg='carnitas')
-
-
-def make_better_taco(name, meat='chicken', veg='lettuce'):
-    print(f"Taco for {name} has {meat} and {veg}.")
-
-make_better_taco("Arnold", "steak", "guacamole")
-make_better_taco('James')
-
-
-print("-"*20)
-def total_for_customer(tacos):
-    """
-    totals the customer's order and returns the amount
-
-    """
-    cost_per_taco = 1.25
-
-    return round((cost_per_taco * tacos) * (1.07), 2)
-
-print(total_for_customer(5))
-arnold = total_for_customer(10)
-drink = 2
-shirt = 25
-print("Arnolds total: $", arnold + drink + shirt)
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:7fa3d11bced02bb1b0521e6a863edb9a900bfe1da96f2c4d4779b1ff05855c00
+size 845
--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-db_password = 'Strongman123!'
+version https://git-lfs.github.com/spec/v1
+oid sha256:2807552be1eb46a18f78d1fcd6769151abce4fae92abad314a8ab9d42a806332
+size 29
